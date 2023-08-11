727fd01d1331e8eb75b0842f3201ad6df93a9c3d
guisantosalves <workguisantos@gmail.com>
2023-08-11 00:09:22 -0300

    mapping my list and improving the screen

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -17,11 +17,15 @@ class AnswerButton extends StatelessWidget {
         onTap();
       },
       style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
           backgroundColor: const Color.fromARGB(255, 33, 1, 95),
           foregroundColor: Colors.white,
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(40))),
-      child: Text(answerText),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -4,4 +4,12 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  // we need this because shuffle update the original array
+  // but here I dont want that
+  List<String> getShuffledAnswers() {
+    final currentList = List.of(answers); // make a copy
+    currentList.shuffle(); // shuffle
+    return currentList;
+  }
 }
